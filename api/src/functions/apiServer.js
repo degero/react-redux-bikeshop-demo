60895be96835fdc051c89c41a663124d83cc1834
degero <chaddotnet@outlook.com>
2026-03-02 10:47:39 +0700

    inital function app test

--- api/src/functions/apiServer.js
+++ api/src/functions/apiServer.js
@@ -4,7 +4,26 @@ app.http('bikes', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-
-      
+        if (request.method === 'GET') {
+            // Handle GET request
+            return {
+                status: 200,
+                body: JSON.stringify([
+                    {
+                        id: 1,
+                        manufacturerId: 1,
+                        model: "Bronson",
+                        price: 6999,
+                        slug: "santa-cruz-bronson",
+                    },
+                ])
+            };
+        } else if (request.method === 'POST') {
+            // Handle POST request
+            return {
+                status: 201,
+                body: bike
+            };
+        }
     }
 });
