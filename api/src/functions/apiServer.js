f209d2788a266ca4aa306be0b5662a28a808e06e
degero <chaddotnet@outlook.com>
2026-03-02 10:59:35 +0700

    cleanup api

--- api/src/functions/apiServer.js
+++ api/src/functions/apiServer.js
@@ -1,5 +1,29 @@
 const { app } = require('@azure/functions');
 
+let bikes = [
+    {
+        id: 1,
+        manufacturerId: 1,
+        model: "Bronson",
+        price: 6999,
+        slug: "santa-cruz-bronson",
+    },
+];
+
+let manufacturers = [
+  {
+    id: 1,
+    name: "Santa Cruz",
+    slug: "santa-cruz"
+  },
+  {
+    id: 2,
+    name: "Yeti",
+    slug: "yeti"
+  },
+];
+
+
 app.http('bikes', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
@@ -8,15 +32,28 @@ app.http('bikes', {
             // Handle GET request
             return {
                 status: 200,
-                body: JSON.stringify([
-                    {
-                        id: 1,
-                        manufacturerId: 1,
-                        model: "Bronson",
-                        price: 6999,
-                        slug: "santa-cruz-bronson",
-                    },
-                ])
+                jsonBody: bikes
+            };
+        } else if (request.method === 'POST') {
+            // Handle POST request
+            return {
+                status: 201,
+                body: bike
+            };
+        }
+    }
+});
+
+
+app.http('manufacturers', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        if (request.method === 'GET') {
+            // Handle GET request
+            return {
+                status: 200,
+                jsonBody: manufacturers
             };
         } else if (request.method === 'POST') {
             // Handle POST request
